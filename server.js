d5d9294c0021d7f38c11546731c5d321cc37c065
Rebecca Blanton <beccaablanton@gmail.com>
2020-12-19 18:59:17 -0800

    added server file,server boiler plate

---
+++ server.js
@@ -0,0 +1,16 @@
+
+var express = require("express");
+var mysql = require("mysql")
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
